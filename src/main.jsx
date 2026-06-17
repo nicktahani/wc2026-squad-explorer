6f15fd0bf122a374937f315ac7dc2f94098d7722
nick t <ns.tahani@gmail.com>
2026-06-16 23:15:06 -0700

    tanstack wiring

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './index.css'
+import App from './App.jsx'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // cache data fetching 
+      staleTime: 1000 * 60 * 60 * 6, // 6 hours
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
+)
