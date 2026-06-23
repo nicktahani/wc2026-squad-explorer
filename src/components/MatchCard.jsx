25b9d31d91db2814723c8a3d85409570ecbcd024
nick t <ns.tahani@gmail.com>
2026-06-22 21:51:25 -0700

    improve MatchCard readability

--- src/components/MatchCard.jsx
+++ src/components/MatchCard.jsx
@@ -3,13 +3,28 @@ import '../styles/MatchCard.css'
 import { formatDate } from '../utils/date'
 
 export default function MatchCard({ match }) {
-  const { team1, team2, team1Code, team2Code, team1Flag, team2Flag, date, time, score, status, ground, round } = match
+  const { 
+    team1, 
+    team2, 
+    team1Code, 
+    team2Code, 
+    team1Flag, 
+    team2Flag, 
+    date, 
+    time, 
+    score, 
+    status, 
+    ground, 
+    round 
+  } = match
   const ft = score?.ft
 
   return (
     <Link to={`/match/${match.id}`} className={`match-card match-card--${status}`}>
       <div className="match-card__row">
-        <span className="match-card__team">{team1Code ? `${team1Flag} ${team1Code}` : team1}</span>
+        <span className="match-card__team">
+          {team1Code ? `${team1Flag} ${team1Code}` : team1}
+        </span>
         <div className="match-card__center">
           {ft ? (
             <span className="match-card__score">{ft[0]}–{ft[1]}</span>
@@ -17,7 +32,9 @@ export default function MatchCard({ match }) {
             <span className="match-card__vs">vs</span>
           )}
         </div>
-        <span className="match-card__team match-card__team--right">{team2Code ? `${team2Flag} ${team2Code}` : team2}</span>
+        <span className="match-card__team match-card__team--right">
+          {team2Code ? `${team2Flag} ${team2Code}` : team2}
+        </span>
       </div>
       <div className="match-card__meta">
         <span className="match-card__meta-line1">
